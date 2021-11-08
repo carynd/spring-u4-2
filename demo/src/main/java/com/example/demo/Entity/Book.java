45b616a35f761c0cf851c52fab4cbe8d95bd4d6a
carynd <caryntech@gmail.com>
2021-11-08 14:44:51 +0530

    Initial commit (continuing here) due to issues

---
+++ demo/src/main/java/com/example/demo/Entity/Book.java
@@ -0,0 +1,18 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Entity;
+
+@Entity
+
+public class Book {
+
+    /*
+    id : 101,
+		name: "Skull Candy Headphones",
+    price: 10000,
+    brand: "Skull Candy",
+    category: "Headphones",
+		registration_number: "2gh856",
+		manufacturer_id: "MAN254
+     */
+}
