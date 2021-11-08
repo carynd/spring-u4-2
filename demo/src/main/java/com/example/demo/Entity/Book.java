26b6e1be4657e0ab8ddbdb2748c582985a87a70d
carynd <caryntech@gmail.com>
2021-11-08 14:46:36 +0530

    initial

--- demo/src/main/java/com/example/demo/Entity/Book.java
+++ demo/src/main/java/com/example/demo/Entity/Book.java
@@ -1,11 +1,16 @@
 package com.example.demo.Entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 
 public class Book {
 
+    @Id
+    private int id;
+    private int price;
+    private
     /*
     id : 101,
 		name: "Skull Candy Headphones",
